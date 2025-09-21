9831ef31ed236ea507301ae304854c05a11bd66b
tvlisboa <tvlisboa@hotmail.com>
2025-09-21 17:20:44 -0300

    alteração dos icones da landing-page

--- src/app/components/SextionFeatures/index.tsx
+++ src/app/components/SextionFeatures/index.tsx
@@ -1,4 +1,9 @@
 import { PiNewspaperFill } from "react-icons/pi";
+import { MdNewReleases } from "react-icons/md"
+import { FiAward } from "react-icons/fi"
+import { CgDanger } from "react-icons/cg";
+import { BiWorld } from "react-icons/bi";
+import { CiSquareMore } from "react-icons/ci";
 import { GridContainer } from "../GridContainer";
 import { ItemFeature } from "./ItemFeature";
 
@@ -23,33 +28,33 @@ export function SectionFeatures() {
                     />
 
                     <ItemFeature
-                        icon={<PiNewspaperFill size={46} color="red" />}
-                        title="Os melhores perfumes de 2025"
-                        text="Seleção inclui fragrâncias versáteis, amadeiradas, cítricas e florais, com opções para diferentes perfis e ocasiões"
+                        icon={<MdNewReleases size={46} color="red" />}
+                        title="Os 7 perfumes masculinos mais desejados de 2025 "
+                        text="Das fragrâncias clássicas ás mais modernas, conheça os perfumes masculinos que conquistaram especialistas e que foram sucesso durante o ano"
                     />
 
                     <ItemFeature
-                        icon={<PiNewspaperFill size={46} color="red" />}
-                        title="Os melhores perfumes de 2025"
-                        text="Seleção inclui fragrâncias versáteis, amadeiradas, cítricas e florais, com opções para diferentes perfis e ocasiões"
+                        icon={<FiAward size={46} color="red" />}
+                        title="5 Marcas de nicho que exalam poder"
+                        text="Descubra perfumarias que criam fragrâncias exclusivas, de alta qualidade e com aromas duradouros, para você se manter perfumada por mais tempo "
                     />
 
                     <ItemFeature
-                        icon={<PiNewspaperFill size={46} color="red" />}
-                        title="Os melhores perfumes de 2025"
-                        text="Seleção inclui fragrâncias versáteis, amadeiradas, cítricas e florais, com opções para diferentes perfis e ocasiões"
+                        icon={<CgDanger size={46} color="red" />}
+                        title="O Boticário amplia linha de cuidados capilares de Dr. Botica para cabelos lisos"
+                        text="A novidade possui tecnologia Tira Nó, em que 9 em cada 10 cuidadores afirmam que o cabelo da criança ficou mais fácil de pentear"
                     />
 
                     <ItemFeature
-                        icon={<PiNewspaperFill size={46} color="red" />}
-                        title="Os melhores perfumes de 2025"
-                        text="Seleção inclui fragrâncias versáteis, amadeiradas, cítricas e florais, com opções para diferentes perfis e ocasiões"
+                        icon={<BiWorld size={46} color="red" />}
+                        title="Natura deixou para trás os planos de conquistar o mundo"
+                        text="Brasileira chegou a ser a quarta maior empresa do mundo no setor de beleza."
                     />
 
                     <ItemFeature
-                        icon={<PiNewspaperFill size={46} color="red" />}
-                        title="Os melhores perfumes de 2025"
-                        text="Seleção inclui fragrâncias versáteis, amadeiradas, cítricas e florais, com opções para diferentes perfis e ocasiões"
+                        icon={<CiSquareMore size={46} color="red" />}
+                        title="Quem cria os perfumes que você usa?"
+                        text="Neste 21 de setembro, Verônica Kato, perfumista exclusiva da Natura, revela os bastidores da profissão rara, além de tendências de perfume para a primavera "
                     />
                 </div>
             </GridContainer>
